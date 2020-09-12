2d9efdeed3ec77366e3ab40e31743f57a3b04946
tika1998 <tika-hovhannisyan@mail.ru>
2020-09-12 13:04:56 +0400

    chnage name

---
+++ js/script.js
@@ -0,0 +1,98 @@
+/* Data type*/
+
+
+/* primicive -   string, number, boolean, null, undifined*/ 
+
+
+var num1 = 9;
+var num2 = 5;  //number 
+
+var num3 = '20.35'; //string 
+
+var num4 = 'hello'; 
+//console.log( Number(num3)) ; 
+
+// console.log(parseFloat(num3)); // 20.35
+// console.log(parseInt(num3));   // 20
+
+// var n = 25.65;
+
+// console.log(Math.round(n)); // 4.7 - 5 / 4.4 - 4
+// console.log(Math.ceil(n));  // 4.4 - 5
+// console.log(Math.floor(n)); // 4.8 - 4
+
+// console.log(Math.pow(8,2))  // 64
+
+// console.log(Math.sqrt(64)); // 8
+
+// console.log(Math.max(21,1,24,3)) // 24
+
+// console.log(Math.min(21,1,24,3)) //  1
+
+// console.log(parseInt(Math.random() * 8)) //  7
+
+//  var n = 25.325;
+
+//  console.log(n.toFixed(2)) // 25.32
+
+
+//  var name = prompt('Enter name');  // NORIK
+//  var name2 = name.toLowerCase(); // norik
+
+// switch(name2) {  // NORIK
+//     case 'norik':
+//         console.log('hello' + ' ' + name2)
+//         break
+//      case 'davit':
+//         console.log('hello' + ' ' + name2)
+//         break 
+//     case 'davit':
+//         console.log('hello' + ' ' + name2)
+//         break
+//      default:
+//         console.log('Sxal anun eee')
+// }
+
+
+
+
+
+
+
+
+// if (name2 === 'NORIK') {
+//     console.log('hello' + ' ' + name2)
+// } else if (name2 === 'DAVID') {
+//     console.log('hello' + ' ' + name2)  
+// } else if (name2 === 'ani') {
+//     console.log('hello' + ' ' + name2)  
+// } else {
+//     console.log('Sxal anun eee')
+// }
+
+// if (name2 === 'norik'|| name2 === 'david' || name2 === 'ani')  {
+//     console.log('hello' + ' ' + name2)
+// } else {
+//     console.log('Sxal anun eee')
+// }
+
+
+
+// var 
+// let
+// const
+
+// let x = 54; //54 
+//     x = 68; // 68
+// console.log(x)
+
+// const y = 98;
+//       y = 100;
+// console.log(y)
+       
+var y = 98;
+    y = 100;
+console.log(y)
+
+
+
